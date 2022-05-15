eb8157fc24bda4b782b32c328a2663855a5c3d46
Vic Lo <ab889721@gmail.com>
2022-05-15 14:53:40 +0800

    add backend api for news

--- hooks/useNewsApi.js
+++ hooks/useNewsApi.js
@@ -1,10 +1,9 @@
 import useSWR from "swr";
-import { NEWS_API_KEY, NEWS_API_DOMAIN } from "../constants";
 import fetcher from "../lib/fetcher";
 
 export function useNewsApi(params) {
   const { searchText = "", page = 1 } = params;
-  const api = `${NEWS_API_DOMAIN}/everything?q=${searchText}&page=${page}&apiKey=${NEWS_API_KEY}`;
+  const api = `/api/news/everything?q=${searchText}&page=${page}`;
   const { data, error } = useSWR(searchText ? api : null, fetcher);
   return {
     data,
